ed9afe6a47b3472e217d069dd0a0f0a81f2ddab4
Ubuntu <ubuntu@ip-172-31-43-135.ap-northeast-2.compute.internal>
2025-02-01 10:16:57 +0000

    update setup.py and config

--- projects/configs/experiment/deploy_bevformer_small.py
+++ projects/configs/experiment/deploy_bevformer_small.py
@@ -3,3 +3,8 @@ _base_ = [
 ]
 
 onnx_file_path = "./onnx_output"
+seed = 0
+checkpoint = "/home/ubuntu/workspace/BEVFormer/ckpts/r101_dcn_fcos3d_pretrain.pth"
+fuse_conv_bn = False
+show = True
+show_dir = "./onnx_output"
